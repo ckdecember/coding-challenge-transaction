768f1612f62d1b84ea75c79e4c5c5d5495042424
carroll kong <github@carrollkong.com>
2019-07-26 19:19:36 -0400

    scaffolding for POST method

--- node.js/server.js
+++ node.js/server.js
@@ -5,6 +5,15 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+app.post('/transfer', function (req, res) {
+   res.send('POST MODE');
+})
+
+/*
+json output response
+{id:transaction_id, from:{id:account, balance:current_balance},to:{id:account,balance:current_balance}, transfered:transfer_amount}
+*/
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port

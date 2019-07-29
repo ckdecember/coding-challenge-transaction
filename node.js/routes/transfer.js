e1e80086f1d6c301b8b2046e974d496a133c24f4
ck <github@carrollkong.com>
2019-07-29 14:30:28 -0400

    cleaned up comments; able to do 2 inserts into transactions

--- node.js/routes/transfer.js
+++ node.js/routes/transfer.js
@@ -25,17 +25,12 @@ router.post('/', async (req, res, next) => {
         return error;
     }
 
-    //db.query('INSERT INTO transactions (id, amount, accountnumber) \
-    //VALUES (DEFAULT, $1::int, $2::text', -amountmoney, fromaccount)
-
-    /*    const { rows } = await query('INSERT INTO transactions (id, amount, accountnumber) VALUES (DEFAULT, $1::int, $2::int)', 
-            [-amountmoney, fromaccount])
- */   
     try {
         await client.query('BEGIN');
         await client.query('INSERT INTO transactions (id, amount, accountnumber) VALUES (DEFAULT, $1::int, $2::int)', 
             [-amountmoney, fromaccount]);
-        //await client.query('UPDATE bar SET foo = 2');
+        await client.query('INSERT INTO transactions (id, amount, accountnumber) VALUES (DEFAULT, $1::int, $2::int)',
+            [amountmoney, toaccount]);
         await client.query('COMMIT');
     } catch (error) {
         try {
@@ -55,64 +50,6 @@ router.post('/', async (req, res, next) => {
     })
  
 })
-
-
-
-//The request should be a POST request to the method /transfer with the payload: {from:account, to:account, amount:money}.
-/*
-CREATE TABLE transactions (
-    id serial primary key,
-    amount numeric,
-    accountnumber numeric
-)
-
-CREATE TABLE balances (
-    accountnumber numeric unique,
-    balance numeric
-)
-
-The response should return the following payload: {id:transaction_id, from:{id:account, balance:current_balance},to:{id:account,balance:current_balance}, transfered:transfer_amount}
-*/
-
-/*
-var transfer_response = {}
-transfer_response['id'] = transaction_id
-var from_balance = from_current_balance
-var to_curbalance = to_current_balance
-var transfered_amount = amountmoney
-*/
-
-/* BEGIN, COMMIT.*/
-//db.query("BEGIN")
-
-//db.query("SELECT * FROM transactions")
-
-/*
-db.query('INSERT INTO transactions (id, amount, accountnumber) \
-    VALUES (DEFAULT, $1::int, $2::text', -amountmoney, fromaccount)
-    .then( (result) =>
-        res.send("HI")
-    )*/
-
-/*db.query('INSERT INTO transactions (id, amount, accountnumber) \
-    VALUES (DEFAULT, $1::int, $2::text', amountmoney, toaccount)*/
-// insert into transactions (id, amount, accountnumber) values (DEFAULT, amountmoney, toaccount )
-
-//WHERE unitname = $1::text', [req.params.unitname])
-
-/*
-db.query('SELECT fields \
-FROM transactions \
-ORDER BY unitname')
-.then((result) => 
-    res.render('units/index', {
-        'scriptvar': "boo",
-        'unitlist': result.rows,
-        'headers': result.fields
-    }
-))
-.catch((err) => console.error('error executing query', err.stack))
-*/
-
+//The response should return the following payload: {id:transaction_id, from:{id:account, balance:current_balance},to:{id:account,balance:current_balance}, transfered:transfer_amount}
 
 module.exports = router

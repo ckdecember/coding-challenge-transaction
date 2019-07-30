26656054b92b13a6c92e88e209ac609933d11f9a
ck <github@carrollkong.com>
2019-07-30 10:35:28 -0400

    duplication protection added! moving to change from weboutput to json output

--- node.js/routes/transfer.js
+++ node.js/routes/transfer.js
@@ -13,87 +13,35 @@ const pool = new pg.Pool({
 
 /* need to add for POST */
 
-//router.post('/', async (req, res, next) => {
-router.post('/', function (req, res, next) {
+router.post('/', async (req, res, next) => {
     var fromaccount = req.body.fromaccount;
     var toaccount = req.body.toaccount;
     var amountmoney = req.body.amountmoney;
     var sessionguard = req.body.sessionguard;
 
-
-    async function sessioncheck() {
-        try {
-            client = await pool.connect()
-        } catch (error) {
-            console.log('A client pool error occurred:', error);
-            return error;
-        }
-
-        try {
-            rows = await client.query('SELECT EXISTS(SELECT 1 from sessionguard WHERE session_number = $1::int)', [sessionguard])
-        } catch (error) {
-            return error;
-        }
-
-        // work with ROWS.
-        if (rows != true) {
-            return rows;
-        }
+    try {
+        client = await pool.connect()
+    } catch (error) {
+        console.log('A client pool error occurred:', error);
+        return error;
     }
 
-    async function updatesessionguard() {
-        try {
-            client = await pool.connect()
-        } catch (error) {
-            console.log('A client pool error occurred:', error);
-            return error;
-        }
-
-        try {
-            await client.query('INSERT INTO sessionguard (session_number, create_time) VALUES ($1::int, now())',
-                [sessionguard]);
-        } catch (error) {
-            console.log('An error occurred:', error);
-            return error;
-        }
+    try {
+        rows = await client.query('SELECT EXISTS(SELECT 1 from sessionguard WHERE session_number = $1::int)', [sessionguard])
+    } catch (error) {
+        return error;
     }
 
-    sessionguardvar = false;
-    rows = sessioncheck();
-    rows.then(function(result) {
-        console.log(result);
-        console.log(result.rows[0]['exists']);
-        if (rows == true)  {
-            sessionguardvar = true;
-        }
-    })
-
-    if (sessionguardvar == true)  {
-        updatesessionguard().then(function(results) {
-            console.log(results)
-        })
-    }
-
-    // right.  need to someone pass a value back out.
-
-    /*
-    console.log("WHY");
-    if (rows) {
+    console.log(rows)
+    
+    if (await rows.rows[0]['exists'] == true) {
+        // just show the page witout doing the transaction
         res.render('transfer', {
-            'title' : "MO MONEY TRANSFERRED - DUPE FOUND",
+            'title' : "MO MONEY TRANSFERRED",
             'fromaccount' : fromaccount,
             'sessionguard' : sessionguard
-        })
-        return;
-    }*/
-
-    async function transaction() {
-        try {
-            client = await pool.connect()
-        } catch (error) {
-            console.log('A client pool error occurred:', error);
-            return error;
-        }
+        })    
+    }  else  {
 
         try {
             await client.query('BEGIN');
@@ -110,21 +58,25 @@ router.post('/', function (req, res, next) {
             }
             console.log('An error occurred:', error);
             return error;
-        } finally {
-            client.release();
         }
+        
+        try {
+            await client.query('INSERT INTO sessionguard (session_number, create_time) VALUES ($1::int, now())',
+                [sessionguard]);
+        } catch (error) {
+            console.log('An error occurred:', error);
+            return error;
+        }
+
+        // retro fit to return just a payload.  json it
+        
+
+        res.render('transfer', {
+            'title' : "MO MONEY TRANSFERRED",
+            'fromaccount' : fromaccount,
+            'sessionguard' : sessionguard
+        })
     }
-
-    transaction();
-
-    // set a value on the form somehow?
-
-    res.render('transfer', {
-        'title' : "MO MONEY TRANSFERRED",
-        'fromaccount' : fromaccount,
-        'sessionguard' : sessionguard
-    })
- 
 })
 //The response should return the following payload: {id:transaction_id, from:{id:account, balance:current_balance},to:{id:account,balance:current_balance}, transfered:transfer_amount}
 
